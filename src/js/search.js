510548fb02c097bc2ee74f98e907fdee429af4d7
Ethmane Didi <ousman.didibela@gmail.com>
2023-02-28 11:25:40 +0100

    Delete unused files

--- src/js/search.js
+++
@@ -1,6 +0,0 @@
-const searchBtn = document.getElementById('search-btn');
-searchBtn.addEventListener('click', function () {
-  const searchText = document.getElementById('search-input').value;
-  // perform search with searchText
-  console.log("Search for '" + searchText + "' was performed.");
-});
